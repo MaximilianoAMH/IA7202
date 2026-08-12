3fe244a0a48bd96adbde8198ca38d4bb59c1ec30
MaximilianoAMH <m.morales.2@ug.uchile.cl>
2026-08-12 17:12:57 -0400

    feat: continuacion lab2

---
+++ labs/lab_2/main.py
@@ -0,0 +1,5 @@
+from agroalerta.sensores import SensorHumedad, SensorTemperatura, SensorViento
+
+st = SensorTemperatura(0, 40)
+sv = SensorViento(25)
+sh = SensorHumedad(85)
--- labs/lab_2/src/agroalerta/sensores.py
+++ labs/lab_2/src/agroalerta/sensores.py
@@ -1,29 +1,51 @@
-class Sensor:
-    def __init__(self, nombre, unidad):
-        self.nombre = nombre
-        self.unidad = unidad
+from abc import ABC, abstractmethod
 
-    def es_riesgo(self, valor: int):
-        return False
+
+class Sensor(ABC):
+    def __init__(self, nombre, unidad):
+        self._nombre = nombre
+        self._unidad = unidad
+
+    @abstractmethod
+    def es_riesgo(self, valor: int) -> bool:
+        return valor
 
 
 class SensorTemperatura(Sensor):
     def __init__(self, maximo, minimo):
         super().__init__("temperatura", "°C")
-        self.maximo = maximo
-        self.minimo = minimo
+        self._maximo = maximo
+        self._minimo = minimo
+
+    def es_riesgo(self, valor: int) -> bool:
+        return valor < self.minimo or valor > self.maximo
+
+    @property
+    def rango_seguro(self):
+        return f"entre {self._minimo} y {self._maximo} {self.unidad}"
 
 
 class SensorViento(Sensor):
     def __init__(self, maximo):
         super().__init__("viento", "km/h")
-        self.maximo = maximo
+        self._maximo = maximo
+
+    def es_riesgo(self, valor: int) -> bool:
+        return valor > self.maximo
+
+    @property
+    def rango_seguro(self):
+        return f"bajo {self._maximo} {self.unidad}"
 
 
 class SensorHumedad(Sensor):
     def __init__(self, maximo):
         super().__init__("humedad", "%")
-        self.maximo = maximo
+        self._maximo = maximo
 
-    def es_riesgo(self, valor: int):
+    def es_riesgo(self, valor: int) -> bool:
         return valor > self.maximo
+
+    @property
+    def rango_seguro(self):
+        return f"bajo {self._maximo} {self.unidad}"
