f9b2a4e848939a14aec4a743ce15e1ed1d692766
MaximilianoAMH <m.morales.2@ug.uchile.cl>
2026-08-14 17:37:42 -0400

    feat: finalizalab2

--- tests/test_sensores.py
+++ tests/test_sensores.py
@@ -1,4 +1,9 @@
-from src.agroalerta.sensores import SensorTemperatura, SensorViento
+from src.agroalerta.reporte import contar_riesgos
+from src.agroalerta.sensores import (
+    SensorHumedad,
+    SensorTemperatura,
+    SensorViento,
+)
 
 # Primer Test: Temperatura bajo 0 es reisgosa
 
@@ -27,3 +32,11 @@ def test_viento_normal():
 
 
 # Cuarto Test: devuelve el conteo esperado para un conjunto pequeño de lecturas escrito a mano en la prueba.
+def test_contar_riegos():
+    lecturas = {"temperatura": [50], "humedad": [100], "viento": [2]}
+    sensores = [SensorTemperatura(0, 40), SensorHumedad(40), SensorViento(10)]
+    contea_riesgos = contar_riesgos(sensores, lecturas)
+
+    assert contea_riesgos["temperatura"] == 1
+    assert contea_riesgos["humedad"] == 1
+    assert contea_riesgos["viento"] == 0
